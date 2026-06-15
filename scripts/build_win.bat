ac0fe49a28e961a77331b3a26c3a4a40e49ad076
SharpRoma <kravtsov.roman600@gmail.com>
2026-06-15 11:36:51 +0300

    fix(windows) починка парсинга версии для установщика

--- scripts/build_win.bat
+++ scripts/build_win.bat
@@ -12,7 +12,7 @@ if not exist "%ICON_PATH%" (
 )
 
 :: --- ЧИТАЕМ ВЕРСИЮ ИЗ CONFIG.PY ---
-for /f "tokens=2 delims=^=" %%a in ('findstr "APP_VERSION" client\config.py') do set RAW_VERSION=%%a
+for /f "tokens=2 delims==" %%a in ('findstr /B "APP_VERSION" client\config.py') do set RAW_VERSION=%%a
 :: Очищаем от кавычек и пробелов
 set APP_VERSION=%RAW_VERSION:"=%
 set APP_VERSION=%APP_VERSION: =%
